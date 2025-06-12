694a146d581ae05df9bb823970ab6fedec1d76b6
Kunal <kunalkharat2004@gmail.com>
2025-06-12 21:34:10 +0530

    feat: fetch toppings based on restaurant ID from search parameters

--- src/app/(home)/components/topping-list.tsx
+++ src/app/(home)/components/topping-list.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import ToppingCard from "./topping-card";
 import { Topping } from "@/lib/types";
+import { useSearchParams } from "next/navigation";
 
 const ToppingList = ({
   selectedToppings,
@@ -10,11 +11,12 @@ const ToppingList = ({
   handleToppingSelect: (topping: Topping) => void;
 }) => {
   const [toppings, setToppings] = React.useState<Topping[]>([]);
+  const searchParams = useSearchParams();
 
   useEffect(() => {
     const fetchToppings = async () => {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/catalog/api/toppings?tenantId=2adeb26a-266e-4ff4-8b0b-ec79cefdfaf7`
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/catalog/api/toppings?tenantId=${searchParams.get("restaurantId")}`,
       );
       if (!response.ok) {
         throw new Error("Failed to load toppings");
@@ -32,7 +34,9 @@ const ToppingList = ({
   return (
     <section className="w-full">
       <div className="mt-6 px-4 sm:px-6 lg:px-0">
-        <h4 className="text-lg font-semibold mb-3">Choose the toppings</h4>
+        {
+          toppings.length !== 0 ? (<h4 className="text-lg font-semibold mb-3">Choose the toppings</h4>):""
+        }
         <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-3 sm:gap-4 lg:gap-6">
           {toppings.map((topping) => (
             <ToppingCard
